5c3402b052fc035bd3f9833ba518a5952311ce77
pedrohmartinz <pedrohmartinso@gmail.com>
2024-01-03 11:30:08 -0300

    Utilizando o input e concatenação.

---
+++ var/variaveis2.py
@@ -0,0 +1,5 @@
+responsavel=input("Nome do responsável:")
+funcionario=input("Nome do funcionário:")
+evento=input("Digite o nome do evento: ")
+valor=float(input("Digite o valor que será recebido de volta: "))
+print("Declaro para o senhor " + responsavel+ ", que o senhor " + funcionario + " esteve presente no evento " + evento + " e gastou o valor de R$ " + str(valor) + " com a entrada.")
