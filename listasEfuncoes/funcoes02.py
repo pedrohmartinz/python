a2d9dac846515846732eddb4664ed5ab1579ef42
pedrohmartinz <pedrohmartinso@gmail.com>
2024-01-09 11:26:35 -0300

    Import de funções teste.

---
+++ listasEfuncoes/funcoes02.py
@@ -0,0 +1,16 @@
+from Capitulo3_Funcoes.funcoes01 import *
+
+minhaLista=[]
+print("Preenchendo")
+preencherInventario(minhaLista)
+print("Exibindo")
+exibirInventario(minhaLista)
+print("Pesquisando")
+localizarPorNome(minhaLista)
+print("Alterando")
+depreciarPorNome(minhaLista, 20)
+print("Excluindo")
+print(excluirPorSerial(minhaLista))
+exibirInventario(minhaLista)
+print("Resumindo")
+resumirValores(minhaLista)
