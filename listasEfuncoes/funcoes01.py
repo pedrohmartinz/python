3815c7fc245c09db3fe4e5a9355213368798decb
pedrohmartinz <pedrohmartinso@gmail.com>
2024-01-09 11:17:25 -0300

    Transformando o código antigo em funções.

---
+++ listasEfuncoes/funcoes01.py
@@ -0,0 +1,31 @@
+def preencherInventario(lista):
+    resp="S"
+    while resp == "S":
+        equipamento=[input("Equipamento: "),
+                     float(input("Valor: ")),
+                     int(input("Número Serial: ")),
+                     input("Departamento: ")]
+        lista.append(equipamento)
+        resp = input("Digite \"S\"para continuar: ").upper()
+
+def exibirInventario(lista):
+    for elemento in lista:
+        print("Nome.........: ", elemento[0])
+        print("Valor........: ", elemento[1])
+        print("Serial.......: ", elemento[2])
+        print("Departamento.: ", elemento[3])
+
+def localizarPorNome(lista):
+    busca=input("\nDigite o nome do equipamento que deseja buscar: ")
+    for elemento in lista:
+        if busca==elemento[0]:
+            print("Valor..: ", elemento[1])
+            print("Serial.:", elemento[2])
+
+def depreciarPorNome(lista, porc):
+    depreciacao=input("\nDigite o nome do equipamento que será depreciado: ")
+    for elemento in lista:
+        if depreciacao==elemento[0]:
+            print("Valor antigo: ", elemento[1])
+            elemento[1] = elemento[1] * (1-porc/100)
+            print("Novo valor: ", elemento[1])
