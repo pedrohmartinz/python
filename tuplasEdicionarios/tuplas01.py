e8c3620f04a1d664234971d2920f807dce350c52
pedrohmartinz <pedrohmartinso@gmail.com>
2024-01-12 10:31:48 -0300

    Tuplas e funções.

---
+++ tuplasEdicionarios/tuplas01.py
@@ -0,0 +1,16 @@
+from Funcoes.tuplas02 import *
+
+usuarios={}
+opcao=perguntar()
+while opcao == "I" or opcao == "P" or opcao == "E":
+    if opcao=="I":
+        inserir(usuarios)
+    elif opcao=="P":
+        pesquisar(usuarios,input("Qual login deseja pesquisar? "))
+    elif opcao == "E":
+        excluir(usuarios,input("Qual login deseja excluir? "))
+    elif opcao == "L":
+        listar(usuarios)
+    else:
+        print("Saindo do programa")
+    opcao = perguntar()
